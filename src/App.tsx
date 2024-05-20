3dc4ee0db92daf7f8b6a1c393041e6a3bd63ce69
RavidYael <ravidyael.githubuser@gmail.com>
2024-05-20 16:13:41 +0300

    feat: add CreatePetForm and PetList components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CreatePetForm from './components/CreatePetForm';
+import PetList from './components/PetList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreatePetForm />
+      <PetList />
     </div>
   );
 }
